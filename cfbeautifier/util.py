6426c94d7549bfbb1fa85016536df2dff88baf08
Naksu <naksu-github-public@marmelandia.com>
2014-11-03 11:58:19 +0200

    Remove unnecessary import

--- cfbeautifier/util.py
+++ cfbeautifier/util.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-import sys
 
 def previous_end_of_line_pos(string, lexpos):
     "Return -1 if at the beginning of string"
